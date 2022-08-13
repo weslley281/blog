fbfdd6b34f9adf4c24e9a6e0f8f9b0afe93b8d0e
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-13 18:06:37 -0400

    posts interativos

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,43 @@ import styles from './App.module.css';
 import { Siderbar } from './components/Siderbar';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: 'https://github.com/diego3g.png',
+        name: 'Diego Fernandes',
+        name: 'Diego Fernandes',
+        role: 'CTO @ Rocketseat',
+      },
+      content: [
+        {
+          type: 'paragraph',
+          content:
+            'Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma cozinha de tipos e embaralhou-a para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas cinco séculos, mas também o salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker,',
+        },
+        { type: 'link', content: 'incluindo versões de Lorem Ipsum' },
+      ],
+      publishedAt: new Date('2022-05-03 20:00:05'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: 'https://github.com/maykbrito.png',
+        name: 'Mayk Brito',
+        role: 'Educator @ Rocketseat',
+      },
+      content: [
+        {
+          type: 'paragraph',
+          content:
+            'Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma cozinha de tipos e embaralhou-a para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas cinco séculos, mas também o salto para a composição eletrônica, permanecendo essencialmente inalterado. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker,',
+        },
+        { type: 'link', content: 'incluindo versões de Lorem Ipsum' },
+      ],
+      publishedAt: new Date('2022-08-06 07:45:56'),
+    },
+  ];
 
   return (
     <div>
@@ -16,16 +52,15 @@ function App() {
       <div className={styles.wrapper}>
         <Siderbar />
         <main>
-          <Post
-            title="Nada, nada e nada"
-            author="Weslley Henrique Vieira Ferraz"
-            content="A sei lá, vou falar qualquer coisa mesmo, mesmo sabendo que não vai dar em nada ningem vai ler mesmo"
-          />
-          <Post
-            title="A sei lá"
-            author="Weslley Henrique Vieira Ferraz"
-            content="A sei lá, vou falar qualquer coisa mesmo, mesmo sabendo que não vai dar em nada ningem vai ler mesmo"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
