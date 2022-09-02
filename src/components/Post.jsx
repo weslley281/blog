2d91f3d6c73f0403a7d44bf7e859cdec83623b23
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-09-02 11:48:17 -0400

    a

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -48,10 +48,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             );
@@ -76,7 +76,7 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
